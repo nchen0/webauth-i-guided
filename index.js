ab6eeaf7a97f917c84e2dfd0c205962714b5a414
nchen0 <nicky.chen0@gmail.com>
2019-10-01 18:41:50 -0400

    add logout

--- index.js
+++ index.js
@@ -1,64 +1,82 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const server = express();
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
 
 const sessionConfig = {
-  name: 'Nicky Authorization Cookie',
-  secret: 'you are protected',
+  name: "Nicky Authorization Cookie",
+  secret: "you are protected",
   cookie: {
     maxAge: 1000 * 60 * 60,
     secure: false,
     httpOnly: true
   },
   resave: false,
-  saveUninitialized: false,
-}
+  saveUninitialized: false
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 server.use(session(sessionConfig));
 
-
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 14);
   user.password = hash;
 
-  db("users").insert(user).then(result => {
-    console.log('result is: ', result);
-    res.status(201).json(user);
-  }).catch(err => {
-    res.status(500).json(err);
-  })
+  db("users")
+    .insert(user)
+    .then(result => {
+      console.log("result is: ", result);
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   const credentials = req.body;
-  db("users").where({username: credentials.username}).first().then(user => {
-    if (user && bcrypt.compareSync(credentials.password, user.password)) {
-      req.session.user = credentials;
-      res.status(200).json({ message: `Welcome ${user.username}!` });
-    }
-    else {
-      res.status(401).json({ message: 'Invalid Credentials' });
-    }
-  }).catch(error => {
-    res.status(500).json(error);
-  });
+  db("users")
+    .where({ username: credentials.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(credentials.password, user.password)) {
+        req.session.user = credentials;
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-server.get('/api/users', protected, (req, res) => {
+server.post("/api/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send("Error logging out");
+      } else {
+        res.send("Logged out");
+      }
+    });
+  }
+});
+
+// Restricted Route
+server.get("/api/users", protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -69,14 +87,11 @@ server.get('/api/users', protected, (req, res) => {
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
 
-
-
-
-// Restrict route
+// To restrict routes:
 function protected(req, res, next) {
   if (req.session && req.session.user) {
     next();
   } else {
-    res.status(401).json({ message: 'you shall not pass!!' });
+    res.status(401).json({ message: "you shall not pass!!" });
   }
 }
